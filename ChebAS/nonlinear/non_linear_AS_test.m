153af7398c7c1f810933917a82f5f4f30e96e1f1
Kevin Aiton <kevinaiton921@gmail.com>
2018-07-26 13:17:30 -0400

    I fixed some bugs with the nonlinear solver

--- ChebAS/nonlinear/non_linear_AS_test.m
+++ ChebAS/nonlinear/non_linear_AS_test.m
@@ -1,7 +1,7 @@
 domain = [0 1;0 1];
 deg_in = [5 5];
 cheb_struct.domain = domain;
-cheb_struct.degs = [5 5];
+cheb_struct.degs = [32 32];
 cheb_struct.split_flag = [true true];
 cheb_struct.cdeg_in = deg_in;
 cheb_struct.tol = 1e-4;
@@ -9,6 +9,8 @@ cheb_struct.tol = 1e-4;
 %Test with 4 patches
 Tree = ChebPatch(cheb_struct);
 %Tree = Tree.split(1);
+%Tree.split(2);
+%Tree.split(1);
 %Tree.split(2);
 
 F = PUchebfun(Tree);
@@ -31,10 +33,15 @@ tol = [1e-10,1e-9];  param = [40, 1000, .5, 0];  % give exact Jacobian
 
 f = @(u) CavityFlow(1,u,Tree);
 Jac = @(u) CavityFlowJacobian(1,u,Tree);
+
 %sol = nsoldAS(zeros(length(F)*3,1),f,Jac,tol,parms);
 
-options = optimoptions('fsolve','SpecifyObjectiveGradient',true);
-sol = fsolve(@(u)sol_and_jac(f,Jac,u),rand(length(F)*3,1),options);
+%fJ = @(u) sol_and_jac(f,Jac,u);
+%sol = nsold(zeros(length(F)*3,1),fJ,tol,parms);
 
-%options = optimoptions(@fsolve,'SpecifyObjectiveGradient',true,'MaxIterations',10000,'FunctionTolerance',1e-4);
+options = optimoptions('fsolve','SpecifyObjectiveGradient',true);
+sol_f = fsolve(@(u)sol_and_jac(f,Jac,u),rand(length(F)*3,1),options);
+
+
+%options = optimoptions(@fsolve,'SpecifyObjectiveGradient',true,'MaxIterations',10000,'FunctionTolerance',1e-9);
 %[s,~,~,~,~] = fsolve(@(x) SimpNonlinear(Tree,x),zeros(length(Tree),1),options);
