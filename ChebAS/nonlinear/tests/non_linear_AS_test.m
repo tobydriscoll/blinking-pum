b17f741b7bb2786037c1c910af7b877552020d8e
Kevin Aiton <kevinaiton921@gmail.com>
2018-11-02 10:57:20 -0400

    testing custom patch structurs

--- ChebAS/nonlinear/tests/non_linear_AS_test.m
+++ ChebAS/nonlinear/tests/non_linear_AS_test.m
@@ -17,44 +17,45 @@ cheb_struct.tol = 1e-4;
 % F = PUchebfun(Tree);
 % F.sample(@(x,y) zeros(size(x)));
 
-F = PUchebfun(@(x,y)atan((x+y)*5),[-1 1;-1 1],'Degree',[33 33],'tol',1e-7);
+%F = PUchebfun(@(x,y)atan((x+y)*5),[-1 1;-1 1],'Degree',[33 33],'tol',1e-7);
+F = PUchebfun(@(x,y)SideBumpFunc(y,[0 1],0.1),[0 1;0 1],'Degree',[33 33],'tol',1e-10);
 F.reset;
 
 setInterpMatrices(F,true);
 
-% Re  = 10;
-% steep = 0.1;
-% f = @(u,leaf) CavityFlow(Re,u,leaf,steep);
-% Jac = @(u,leaf) CavityFlowJacobian(Re,u,leaf);
+Re  = 10;
+steep = 0.1;
+f = @(u,leaf) CavityFlow(Re,u,leaf,steep);
+Jac = @(u,leaf) CavityFlowJacobian(Re,u,leaf);
+
+F.Setvalues(@(x,y)SideBumpFunc(y,[0 1],steep)); %set values
+F.sample(@(x,y)SideBumpFunc(y,[0 1],steep)); %set coeffs
+u = F.Getvalues();
+Fy = diff(F,2,1);
+w = -(Fy.Getvalues());
+v = zeros(length(F),1);
+init = [u;v;w];
+
+% bound_f = @(x,y) atan((cos(pi*3/16)*x+sin(pi*3/16)*y)*1);
+% nu = 1/2;
+% f = @(u,leaf) Burgers(u,leaf,nu,bound_f);
+% Jac = @(u,leaf) BurgersJacobian(u,leaf,nu);
+% F.Setvalues(bound_f);
+% init = F.Getvalues();
+
+ %tic;
+ %[ sol,normres1,normstep1,numgm1 ] = NSKsolver(f,Jac,init,F,[1e-10 1e-10]);
+ %toc
+
 % 
-% F.Setvalues(@(x,y)SideBumpFunc(y,[0 1],steep)); %set values
-% F.sample(@(x,y)SideBumpFunc(y,[0 1],steep)); %set coeffs
-% u = F.Getvalues();
-% Fy = diff(F,2,1);
-% w = -(Fy.Getvalues());
-% v = zeros(length(F),1);
-% init = [u;v;w];
-
-bound_f = @(x,y) atan((cos(pi*3/16)*x+sin(pi*3/16)*y)*1);
-nu = 1/2;
-f = @(u,leaf) Burgers(u,leaf,nu,bound_f);
-Jac = @(u,leaf) BurgersJacobian(u,leaf,nu);
-F.Setvalues(bound_f);
-init = F.Getvalues();
-
-% tic;
-% [ sol,normres1,normstep1,numgm1 ] = NSKsolver(f,Jac,init,F,[1e-10 1e-10]);
-% toc
-
-% 
-%  tic;
-% [ sol,normres2,normstep2,numgm2,normresf2 ] = SNKsolver(f,Jac,init,F,[1e-10 1e-10]);
-% toc
-
-tic;
-[ sol,normres3,normstep3,numgm3 ] = SNK2levelsolver(f,Jac,init,F,[1e-10 1e-10],1e-3,2);
+  tic;
+ [ sol,normres2,normstep2,numgm2,normresf2 ] = SNKsolver(f,Jac,init,F,[1e-10 1e-10]);
 toc
 
-solr = reshape(sol,length(F),1);
+%tic;
+%[ sol,normres3,normstep3,numgm3 ] = SNK2levelsolver(f,Jac,init,F,[1e-10 1e-10],1e-3,2);
+%toc
+
+solr = reshape(sol,length(F),3);
 F.sample(solr(:,1));
 plot(F);
