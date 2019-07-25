ef614b764403ea73778d726ef19d2a53d8ab9b8e
Toby Driscoll <driscoll@udel.edu>
2019-07-25 15:16:36 -0400

    New tests

---
+++ tests/custom_regions.m
@@ -0,0 +1,76 @@
+addpath ..
+addpath ../PUChebfun
+
+param.domain = [-1 1;-1 1];
+param.degs = [36 36];
+param.cdegs = [9 9];
+param.split_flag = [true true];
+param.tol = 1e-4;
+param.odetol = 1e-4;
+
+tspan = [0 5.258];
+param.percentClosed = 0.7;
+param.pA = 2.14e-6;
+param.pS = 6.92e-5;
+param.h_e = 2;
+param.BoundaryH = 13;
+param.initvolume = 34;
+param.fluxvolume = 0;
+
+%Test with 4 patches
+Tree = ChebPatch(param);
+overlap = 0.2;
+
+%This replaces Tree with one leaf overlap away from a boundary
+%| |      |
+%| |      |
+%| |      |
+%| |      |
+Tree = Tree.split(2,false,overlap);
+
+%This does the same but with the larger leaf
+%| |     | |
+%| |     | |
+%| |     | |
+%| |     | |
+Tree.children{2} = Tree.children{2}.split(2,false,overlap);
+
+
+%This does the same but with the larger leaf
+%| |     | |
+%| |     | |
+%| |_ _ _| |
+%| |     | |
+Tree.children{2}.children{1} = Tree.children{2}.children{1}.split(1,false,overlap);
+
+
+%This does the same but with the larger left over leaf
+%| |_ _ _| |
+%| |     | |
+%| |_ _ _| |
+%| |     | |
+Tree.children{2}.children{1}.children{2} = Tree.children{2}.children{1}.children{2}.split(1,false,overlap);
+
+Tree.clean();
+
+H = PUchebfun(Tree);
+
+%H = PUchebfun(@(x,y)exp(-x.^20./(1-x.^20)).*exp(-y.^20./(1-y.^20)),[-1 1;-1 1],'Degree',[20 20],'CoarseDegree',[9 9],'tol',1e-3);
+%H.reset();
+
+H.sample(@(x,y) zeros(size(x)));
+P = H.copy();
+
+setInterpMatrices(H,false);
+setInterpMatrices(P,false);
+
+load initcond_pcl7.mat
+[Blinks,M,y0,dy0] = initialize(H,P,param);
+
+opt = odeset('mass',M,'reltol',param.odetol,'abstol',param.odetol,...
+    'initialstep',1e-12,'initialslope',dy0);
+
+[t,U] = ASode15s(false,Blinks,tspan,y0,{H,P},1,opt);
+
+
+
---
+++ tests/eight_regions.m
@@ -0,0 +1,49 @@
+addpath ..
+addpath ../PUChebfun
+
+param.domain = [-1 1;-1 1];
+param.degs = [15 15];
+param.cdegs = [8 8];
+param.split_flag = [true true];
+param.tol = 1e-4;
+param.odetol = 1e-4;
+
+tspan = [0 5.258];
+param.percentClosed = 0;
+param.pA = 2.14e-6;
+param.pS = 6.92e-5;
+param.h_e = 2;
+param.BoundaryH = 13;
+param.initvolume = 34;
+param.fluxvolume = 0;
+
+%Test with 16 patches
+Tree = ChebPatch(param);
+Tree = Tree.split(1);
+Tree.children{1} = split(Tree.children{1},2);
+Tree.children{2} = split(Tree.children{2},2);
+Tree.children{1}.children{1} = split(Tree.children{1}.children{1},1);
+Tree.children{1}.children{2} = split(Tree.children{1}.children{2},1);
+Tree.children{2}.children{1} = split(Tree.children{2}.children{1},1);
+Tree.children{2}.children{2} = split(Tree.children{2}.children{2},1);
+
+Tree.clean();
+
+H = PUchebfun(Tree);
+
+H.sample(@(x,y) zeros(size(x)));
+P = H.copy();
+
+setInterpMatrices(H,false);
+setInterpMatrices(P,false);
+
+load initcond_pcl0.mat
+[Blinks,M,y0,dy0] = initialize(H,P,param,finalstate);
+
+opt = odeset('mass',M,'reltol',param.odetol,'abstol',param.odetol,...
+    'initialstep',1e-12,'initialslope',dy0);
+
+[t,U] = ASode15s(true,Blinks,tspan,y0,{H,P},1,opt);
+
+
+
